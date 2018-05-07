58a711803aac561a50e4571cb9430f2656965fb4
Denis Maslakov <maslakov_denis@mail.ru>
2018-05-07 15:49:27 +0400

    Init Eureka Server

---
+++ src/test/java/com/matlab/server/MatlabServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.matlab.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MatlabServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
